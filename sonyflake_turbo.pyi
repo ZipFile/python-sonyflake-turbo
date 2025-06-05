d2f8e17c45cc603938bb2816e38d3eb0eb63163d
Anatolii Aniskovych <lin.aaa.lin@gmail.com>
2025-06-05 13:19:51 +0000

    Initial Implementation

---
+++ sonyflake_turbo.pyi
@@ -0,0 +1,19 @@
+from typing import Optional
+
+try:
+    from typing import Self
+except ImportError:
+    from typing_extensions import Self
+
+SONYFLAKE_EPOCH: int
+SONYFLAKE_SEQUENCE_BITS: int
+SONYFLAKE_SEQUENCE_MAX: int
+SONYFLAKE_MACHINE_ID_BITS: int
+SONYFLAKE_MACHINE_ID_MAX: int
+SONYFLAKE_MACHINE_ID_OFFSET: int
+SONYFLAKE_TIME_OFFSET: int
+
+class SonyFlake:
+    def __init__(self, *machine_id: int, start_time: Optional[int] = None): ...
+    def __iter__(self) -> Self: ...
+    def __next__(self) -> int: ...
