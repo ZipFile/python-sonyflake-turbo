f1fdfc72096d38cc62cacb40f78bb830f22dc319
Anatolii Aniskovych <lin.aaa.lin@gmail.com>
2025-10-11 15:28:43 +0000

    Add MSVC support

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 import sysconfig
+from typing import Any, Dict, List, Optional, Tuple
 
 from setuptools import Extension, setup
 
-if sysconfig.get_config_var("Py_GIL_DISABLED"):
-    options = {}
-    define_macros = []
-    py_limited_api = False
+options: Dict[str, Any] = {}
+define_macros: List[Tuple[str, Optional[str]]] = [("_POSIX_C_SOURCE", "200809L")]
+py_limited_api: bool = not sysconfig.get_config_var("Py_GIL_DISABLED")
+cflags: List[str] = []
+
+if sysconfig.get_platform().startswith("win"):
+    cflags.append("/utf-8")
+    cflags.append("/std:c17")
+    cflags.append("/Zc:preprocessor")
+    cflags.append("/experimental:c11atomics")
 else:
-    options = {"bdist_wheel": {"py_limited_api": "cp38"}}
-    define_macros = [("Py_LIMITED_API", "0x030800f0")]
-    py_limited_api = True
+    cflags.append("-std=c17")
+
+if py_limited_api:
+    options["bdist_wheel"] = {"py_limited_api": "cp38"}
+    define_macros.append(("Py_LIMITED_API", "0x030800f0"))
 
 setup(
     options=options,
@@ -20,10 +29,11 @@ setup(
             sources=[
                 "src/sonyflake_turbo/_sonyflake.c",
                 "src/sonyflake_turbo/sonyflake.c",
-                "src/sonyflake_turbo/machine_ids.c"
+                "src/sonyflake_turbo/machine_ids.c",
             ],
             define_macros=define_macros,
             py_limited_api=py_limited_api,
+            extra_compile_args=cflags,
         ),
     ],
 )
